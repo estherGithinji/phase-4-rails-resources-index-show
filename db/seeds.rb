d59a96589effc2a5e26c3e4a13cdd3f69f7f6c11
esther <esther.githinji@student.moringaschool.com>
2023-03-14 22:28:46 +0300

    resource routing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+puts '🌱 seeding....'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Bird.create!(name: "Black-Capped Chickadee", species: "Poecile Atricapillus")
+Bird.create!(name: "Grackle", species: "Quiscalus Quiscula")
+Bird.create!(name: "Common Starling", species: "Sturnus Vulgaris")
+Bird.create!(name: "Mourning Dove", species: "Zenaida Macroura")
+
+puts '🌱 done seeding....'
